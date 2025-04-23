6051c0abb8b1a2ebac61b26ea9131e4ee37393e2
esteban_castañeda <esteban2306dd@gmail.com>
2025-04-23 15:56:43 -0500

    add api class and create modal of delete buttom

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { PopupWithForm } from '../components/PopupWithForm.js';
 import { PopupWithImage } from '../components/PopupWithImage.js';
 import { UserInfo } from '../components/UserInfo.js';
 import Section from '../components/Section.js';
+import Api from '../components/api.js';
 import {
     galeryItems,
     editButton,
@@ -26,7 +27,7 @@ const popupimage = new PopupWithImage('.popupimg', (data) => {
 });
 
 const renderGalery = new Section({
-    items: galeryItems,
+    items: [],
     renderer: (item) => {
         const card = new Card(
             item,
@@ -35,12 +36,32 @@ const renderGalery = new Section({
                 popupimage.open({ src: link, alt: name, caption: name })
                 popupimage.setEventListeners();
             })
+
         return card.addCard(item);
     }
 }, '#galery__content'
 );
 
+
 renderGalery.rendererItems();
+
+fetch('https://around-api.es.tripleten-services.com/v1/cards/', {
+    method: 'GET',
+    headers: {
+        "content-type": "application/json; charset=UTF-8",
+        authorization: '838f9adb-9e40-4b54-8266-23d124ff4365'
+
+    },
+})
+    .then(res => {
+        if (!res.ok) throw new Error(`Error: ${res.status}`);
+        return res.json();
+    })
+    .then(cards => {
+        renderGalery._items = cards;
+        renderGalery.rendererItems();
+    })
+    .catch(err => console.error('Error al cargar las tarjetas:', err));
 
 const popupForm = new PopupWithForm('.modal__add', (data) => {
     const nombre = data.title;
@@ -81,4 +102,18 @@ editButton.addEventListener('click', () => {
     document.querySelector('#nombre').value = name;
     document.querySelector('#descripcion').value = about;
     popupEditForm.open();
+
+
 });
+
+fetch('https://around-api.es.tripleten-services.com/v1/users/me', {
+    method: 'GET',
+    headers: {
+        authorization: "838f9adb-9e40-4b54-8266-23d124ff4365"
+    }
+}
+)
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+
